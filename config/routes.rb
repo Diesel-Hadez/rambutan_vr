d8ea78cee9b7d68fabfb21ce8c479f5f604ca73b
Diesel-Hadez <Diesel-Hadez@users.noreply.github.com>
2019-06-24 18:13:10 +0800

    Added some frontend functionality w/ bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  root 'sessions#new'
   resources :users
   resources :movies
   resources :movieitems
